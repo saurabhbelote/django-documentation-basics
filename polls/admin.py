9c81672823cacc17ee9d436b5cad14643c50d120
saurabh belote <saurabhbelote112@gmail.com>
2021-09-13 10:23:16 -0700

    first commit

---
+++ polls/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models import Question,Choice
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question)
+admin.site.register(Choice)
